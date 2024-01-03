27e90585440a1935d2296ae3412fe29999a22f78
RAJATHADU <rajatharajju21116@gmail.com>
2024-01-03 10:28:41 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function testFun(){
+    alert("RAJATHA");
+}
+
+document.getElementById("btn").onclick=testFun; //default calling
+
+// testFun() it will display output 
+// if we do not use brackets then it is default calling
